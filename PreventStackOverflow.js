8eebe099df338bfe424dee347d12b61dc3343b1a
Cansu Tuzmen <52562509+cansutuzmen@users.noreply.github.com>
2024-03-27 21:13:17 +0300

    Create PreventStackOverflow.js

---
+++ PreventStackOverflow.js
@@ -0,0 +1,29 @@
+// How to prevent stack overflow using recursion?
+const list = new Array(120000).join('1.1').split('.');
+function removeItemsFromList() {
+   var item = list.pop();
+
+   if (item) {
+       removeItemsFromList();
+   } 
+};
+removeItemsFromList();
+
+
+// ANSWER:
+// use JS Runtime instead
+const list = new Array(120000).join('1.1').split('.');
+
+function removeItemsFromList() {
+   var item = list.pop();
+
+   if (item) {
+       setTimeout( removeItemsFromList, 0 );
+   } else {
+       console.log("done");
+   }
+};
+
+removeItemsFromList();
+
+console.log("done");
