2e38389090e40acd59ee4ba6516f84d30a3402fb
Cansu Tuzmen <52562509+cansutuzmen@users.noreply.github.com>
2024-03-28 12:24:43 +0300

    Create UseStrict.js

---
+++ UseStrict.js
@@ -0,0 +1,20 @@
+function a() {
+  value = 50;
+  return value;
+}
+
+a();
+
+// Output
+// 50
+
+'use strict'
+function a() {
+  value = 50;
+  return value;
+}
+
+a();
+
+// Output
+// ReferenceError: value is not defined
