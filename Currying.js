b3a72a6cd3afbcdc66e936162cf227923e5763c1
Cansu Tuzmen <52562509+cansutuzmen@users.noreply.github.com>
2024-03-28 15:48:57 +0300

    Create Currying.js

---
+++ Currying.js
@@ -0,0 +1,13 @@
+function multiply(a,b) {
+  return a*b;
+}
+
+let multiplyByTwo = multiply.bind(this, 2);
+console.log(multiplyByTwo(4));
+
+let multiplyByTen = multiply.bind(this, 10);
+console.log(multiplyByTen(4));
+
+// Output
+// 8
+// 40
