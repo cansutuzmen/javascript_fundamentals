21c694973d4d1aa336daf52661e18951e0bf9f5b
Cansu Tuzmen <52562509+cansutuzmen@users.noreply.github.com>
2024-03-28 19:59:44 +0300

    Create TypeCoersion.js

---
+++ TypeCoersion.js
@@ -0,0 +1,20 @@
+console.log(false == "");  
+console.log(false == []);   
+console.log(false == {});   
+console.log("" == 0 );      
+console.log("" == []);      
+console.log("" == {});      
+console.log(0 == []);       
+console.log(0 == {});       
+console.log(0 == null); 
+
+// Output
+// true
+// true
+// false
+// true
+// true
+// false
+// true
+// false
+// false
