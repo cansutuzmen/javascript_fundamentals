af05fa64a0eaa19fe89fae97321c8b07a7e913b7
Cansu Tuzmen <cansutuzmen@Cansus-MacBook-Pro.local>
2024-04-05 14:43:52 +0300

    #async_js, examples added

---
+++ asyncronous_js/Sequence.js
@@ -0,0 +1,18 @@
+const promisify = (item, delay) =>
+  new Promise((resolve) => setTimeout(() => resolve(item), delay));
+
+const a = () => promisify('a', 100);
+const b = () => promisify('b', 5000);
+const c = () => promisify('c', 3000);
+
+async function sequence() {
+  const output1 = await a();
+  const output2 = await b();
+  const output3 = await c();
+  return `sequence is done ${output1} ${output2} ${output3}`;
+}
+
+sequence().then(console.log);
+
+// Output
+// sequence is done a b c
